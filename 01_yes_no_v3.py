20b1242e2e1c9bc29c3046725aaf79e8672cb8cb
laua5 <laua5@middleton.school.nz>
2023-05-08 11:00:15 +1200

    Create 01_yes_no_v3.py

---
+++ 01_yes_no_v3.py
@@ -0,0 +1,25 @@
+"""Maori Quiz Yes/No
+Puts the code created in v2 into a loop to make testing easier and more
+efficient.
+"""
+
+
+show_instructions = ""
+while show_instructions != "x":
+    # Ask the user if they have played before
+    show_instructions = input("Have you played this game before?: ").lower()\
+        .strip()
+
+    # If they say yes, output 'Program Continues'
+    if show_instructions == "yes" or show_instructions == "y":
+        print("program continues")
+
+    # If they say no, output "Display Instructions"
+    elif show_instructions == "no" or show_instructions == "n":
+        print("Display instructions")
+
+    # Otherwise - Show error
+    else:
+        print("Please answer 'yes' or 'no'")
+
+    print(f"You entered '{show_instructions}'")
