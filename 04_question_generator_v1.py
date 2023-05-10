fabcf433f5769276f3dd16a53f96aa491c2119bd
laua5 <laua5@middleton.school.nz>
2023-05-10 22:34:20 +1200

    Create 04_question_generator_v1.py

---
+++ 04_question_generator_v1.py
@@ -0,0 +1,26 @@
+"""Component 3 - question_generator v2
+Generates all questions and each question only appears once
+"""
+
+
+import random
+
+
+# List for easy difficulty
+easy_numbers = [["tahi", "1"], ["rua", "2"], ["toru", "3"], ["wha", "4"],
+                ["rima", "5"], ["ono", "6"], ["whitu", "7"], ["waru", "8"],
+                ["iwa", "9"], ["tekau", "10"]]
+
+
+# List for hard difficulty
+hard_numbers = [["tekau ma tahi ", "11"], ["tekau ma rua", "12"],
+                ["tekau ma toru", "13"], ["tekau ma wha", "14"],
+                ["tekau ma rima", "15"], ["tekau ma ono", "16"],
+                ["tekau ma whitu", "17"], ["tekau ma waru", "18"],
+                ["tekau ma iwa", "19"], ["rua tekau", "20"]]
+
+for item in range(10):
+    easy_number = random.choice(easy_numbers)
+
+    # can wrap output making it easier to screenshot
+    print(easy_number, end='\t')
