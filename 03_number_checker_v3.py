e559759e33a4ee333540268e7e057864b6ea2f73
laua5 <laua5@middleton.school.nz>
2023-05-11 13:38:23 +1200

    Update 03_number_checker_v3.py

--- 03_number_checker_v3.py
+++ 03_number_checker_v3.py
@@ -1,5 +1,5 @@
 """ Component 2 (difficulty level)
-Changing v3 into a function
+Changing v2 into a function
 Also needed to change user_balance to the more generic variable name 'response'
 and to change the condition and position of the number comparison into a loop
 to make the function recyclable"""
