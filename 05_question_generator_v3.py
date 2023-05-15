ead8833e1e6f614185a750ecbeae851a9904793c
laua5 <laua5@middleton.school.nz>
2023-05-16 11:11:30 +1200

    Update 05_question_generator_v3.py

--- 05_question_generator_v3.py
+++ 05_question_generator_v3.py
@@ -1,5 +1,6 @@
 """ Component 3 - question generator v3
-Generates a number randomly from the lists  """
+Sets difficulty level to hard and prints all ten numbers (randomly)
+ on a separate line as well as the question number"""
 
 
 import random
@@ -18,35 +19,18 @@ hard_numbers = [["tekau ma tahi ", "11"], ["tekau ma rua", "12"],
                 ["tekau ma whitu", "17"], ["tekau ma waru", "18"],
                 ["tekau ma iwa", "19"], ["rua tekau", "20"]]
 
-# Main Routine
-level = int(input("Choose a level - 1 for easy, 2 for hard: "))
 random.shuffle(hard_numbers)  # Shuffles the list so that there are no repeats
 random.shuffle(easy_numbers)  # Shuffles the list so that there are no repeats
 
 
 # Sets list to hard_numbers
-if level == "2":
-    print(f"Number 1:{(hard_numbers[1])} ")
-    print(f"Number 2:{(hard_numbers[2])} ")
-    print(f"Number 3:{(hard_numbers[3])} ")
-    print(f"Number 4:{(hard_numbers[4])} ")
-    print(f"Number 5:{(hard_numbers[5])} ")
-    print(f"Number 6:{(hard_numbers[6])} ")
-    print(f"Number 7:{(hard_numbers[7])} ")
-    print(f"Number 8:{(hard_numbers[8])} ")
-    print(f"Number 9:{(hard_numbers[9])} ")
-    print(f"Number 10:{(hard_numbers[0])} ")
-
-
-# Sets list to easy_numbers
-else:
-    print(f"Number 1:{(easy_numbers[1])} ")
-    print(f"Number 2:{(easy_numbers[2])} ")
-    print(f"Number 3:{(easy_numbers[3])} ")
-    print(f"Number 4:{(easy_numbers[4])} ")
-    print(f"Number 5:{(easy_numbers[5])} ")
-    print(f"Number 6:{(easy_numbers[6])} ")
-    print(f"Number 7:{(easy_numbers[7])} ")
-    print(f"Number 8:{(easy_numbers[8])} ")
-    print(f"Number 9:{(easy_numbers[9])} ")
-    print(f"Number 10:{(easy_numbers[0])} ")
+print(f"Number 1:{(hard_numbers[1])} ")
+print(f"Number 2:{(hard_numbers[2])} ")
+print(f"Number 3:{(hard_numbers[3])} ")
+print(f"Number 4:{(hard_numbers[4])} ")
+print(f"Number 5:{(hard_numbers[5])} ")
+print(f"Number 6:{(hard_numbers[6])} ")
+print(f"Number 7:{(hard_numbers[7])} ")
+print(f"Number 8:{(hard_numbers[8])} ")
+print(f"Number 9:{(hard_numbers[9])} ")
+print(f"Number 10:{(hard_numbers[0])} ")
