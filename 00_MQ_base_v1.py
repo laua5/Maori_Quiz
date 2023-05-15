d2957b333669fa07cf293a3437609e190be11cac
laua5 <laua5@middleton.school.nz>
2023-05-16 11:11:33 +1200

    Update 00_MQ_base_v1.py

--- 00_MQ_base_v1.py
+++ 00_MQ_base_v1.py
@@ -1,5 +1,5 @@
 """Maori Quiz base component
-Components are added after they have been tested and created"""
+Components are added here after they have been tested and created"""
 
 
 # Functions go here
@@ -23,6 +23,15 @@ def yes_no(question_text):
         else:
             print("Please answer 'yes' or 'no'")
 
+
+# Function to display instructions
+def instructions():
+    print("**** How to Play ****")
+    print()
+    print("The rules of the game will go here")
+    print()
+
+
 # Number Checking function
 def num_check(question, low, high):
     error = "That was not a valid input\n" \
@@ -42,3 +51,19 @@ def num_check(question, low, high):
 
         except ValueError:
             print(error)
+
+
+# Main routine goes here.....
+played_before = yes_no("Have you played this game before? ")
+
+if played_before == "No":
+    instructions()
+
+
+# Ask user what difficulty level they would like to play with
+difficulty_level = num_check("What difficulty level would you like to play at?"
+                             " (1 for easy), (2 for hard) ", 1, 2)
+if difficulty_level == 2:
+    print(f"You are playing at hard difficulty")
+else:
+    print(f"You are playing at easy difficulty")
